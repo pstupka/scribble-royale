0c9108a7661ee8a6b451df5a1c8d158a1fd7cf4a
Patryk Stupka <patryk.stupka@gmail.com>
2022-08-11 00:04:15 +0200

    State Machine WIP

---
+++ scenes/actors/player/states/motion/on-ground/on-ground-state.gd
@@ -0,0 +1,11 @@
+class_name GroundState
+extends MotionState
+
+# warning-ignore-all:unused_class_variable
+var speed = 0.0
+var velocity = Vector2()
+
+func handle_input(event):
+	if event.is_action_pressed("jump"):
+		emit_signal("finished", "jump")
+	return .handle_input(event)
