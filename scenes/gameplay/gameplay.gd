0bce00289d041e9ee2228b33c7a144cf2a867d0b
Patryk Stupka <patryk.stupka@gmail.com>
2022-08-11 00:04:15 +0200

    State Machine WIP

--- scenes/gameplay/gameplay.gd
+++ scenes/gameplay/gameplay.gd
@@ -12,7 +12,7 @@ func pre_start(params):
 		for key in params:
 			var val = params[key]
 			printt("", key, val)
-	$Sprite.position = Game.size / 2
+
 	print("Processing...")
 	yield(get_tree().create_timer(2), "timeout")
 	print("Done")
@@ -21,9 +21,3 @@ func pre_start(params):
 # `start()` is called when the graphic transition ends.
 func start():
 	print("gameplay.gd: start() called")
-
-
-func _process(delta):
-	elapsed += delta
-	$Sprite.position.x = Game.size.x / 2 + 150 * sin(2 * 0.4 * PI * elapsed)
-	$Sprite.position.y = Game.size.y / 2 + 100 * sin(2 * 0.2 *  PI * elapsed)
