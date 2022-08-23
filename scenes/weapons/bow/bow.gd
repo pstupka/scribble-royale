3240a21454175a0c3c8ab21245fa94e62d71d83c
Patryk Stupka <patryk.stupka@gmail.com>
2022-08-23 09:19:41 +0200

    Arrow tuning
    - Change physics engine to godot physics
    - mouse + keyboard controls

--- scenes/weapons/bow/bow.gd
+++ scenes/weapons/bow/bow.gd
@@ -2,7 +2,8 @@ extends Weapon
 
 
 onready var arrow_scene: PackedScene = preload("res://scenes/bullets/arrow/arrow.tscn")
-export var shoot_strength = 2000
+export var shoot_strength = 3000
+
 
 func _input(event: InputEvent) -> void:
 	if event.is_action_pressed("shoot") and can_attack:
@@ -20,7 +21,8 @@ func drop() -> void:
 
 func spawn_arrow() -> void:
 	var arrow_instance = arrow_scene.instance()
+	var shoot_direction = Vector2.RIGHT.rotated(global_rotation).normalized()
 	get_tree().current_scene.add_child(arrow_instance)
 	arrow_instance.global_transform = global_transform
-	arrow_instance.apply_impulse(Vector2.ZERO,Vector2(3,-1).normalized()*shoot_strength)
+	arrow_instance.apply_impulse(Vector2.ZERO, shoot_direction * shoot_strength)
 	
