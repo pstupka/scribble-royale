dd3243c3da50ba66074f8b429b40e2e4810bb118
Patryk Stupka <patryk.stupka@gmail.com>
2022-08-27 00:37:58 +0200

    Tweak footsteps and rename particle script

--- scenes/actors/player/states/motion/jump-state.gd
+++ scenes/actors/player/states/motion/jump-state.gd
@@ -5,7 +5,6 @@ func enter(_msg := {}):
 	player._velocity.y = -player.max_jump_velocity
 
 
-
 func handle_input(event: InputEvent) -> void:
 	if event.is_action_released("jump_p%s" % player.player_id)\
 		and player._velocity.y < -player.min_jump_velocity:
