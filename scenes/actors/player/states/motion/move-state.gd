58ca1cc532612360b38cabae67395870fbbb6c75
pstupka <patryk.stupka@xperi.com>
2022-09-13 15:12:20 +0200

    Tweak footstep and get rid off footstep timer

--- scenes/actors/player/states/motion/move-state.gd
+++ scenes/actors/player/states/motion/move-state.gd
@@ -1,18 +1,12 @@
 extends PlayerState
 
-var footstep_timer: Timer = Timer.new()
 export(float) var acceleration_weight: = 0.1
 
 func _ready() -> void:
-	add_child(footstep_timer)
-	footstep_timer.connect("timeout", self, "_on_footstep_timer_timeout")
-	footstep_timer.autostart = false
-	footstep_timer.wait_time = 0.2
-	
+	pass
 
 func enter(_msg := {}):
 	player.get_node("AnimationPlayer").play("move")
-	footstep_timer.start()
 	if state_machine.previous_state.name == "Fall":
 		player.spawn_footstep()
 	player.multi_jump_reset()
@@ -33,10 +27,6 @@ func physics_update(delta):
 	if not player.is_on_floor():
 		state_machine.transition_to("Fall", {"coyote_jump": true})
 
+
 func exit() -> void:
 	player.get_node("AnimationPlayer").stop(true)
-	footstep_timer.stop()
-
-
-func _on_footstep_timer_timeout() -> void:
-	player.spawn_footstep()
